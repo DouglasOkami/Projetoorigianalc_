51cbc3d4f51c4883764f920a02269becdec2368c
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-25 21:24:38 -0300

    Rename folder from c# to csharp

---
+++ csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Digite uma frase do seu gosto");
+            int duas = int.Parse(Console.ReadLine());
+        }
+    }
+}
