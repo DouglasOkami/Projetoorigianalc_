a13d4a68951e3b7288826c3b5dbc9bd49a4017f3
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-25 21:17:06 -0300

    atualização

--- Criando variaveis /Program.cs
+++ Criando variaveis /Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace Criando_variaveis
+class Programa
 {
-    class Program
-    {
-        static void Main(string[] args)
+    static void Main(String[] args)
+    {   Console.WriteLine("Digite o número do mês");
+        int mes = int.Parse(Console.ReadLine());
+
+        switch (mes)
         {
-            Console.WriteLine("Hello World!");
+            case 1:
+                Console.WriteLine("O mês é Janeiro");
+                break;
+            case 2:
+                Console.WriteLine("O mês é Fevereiro");
+                break;
+            case 3:
+                Console.WriteLine("O mês é Março");
+                break;
+            case 4:
+                Console.WriteLine("O mês é Abril");
+                break;
+            case 5:
+                Console.WriteLine("O mês é Maio");
+                break;
+            case 6:
+                Console.WriteLine("O mês é Junho");
+                break;
+            case 7:
+                Console.WriteLine("O mês é Julho");
+                break;
+            case 8:
+                Console.WriteLine("O mês é Agosto");
+                break;
+            case 9:
+                Console.WriteLine("O mês é Setembro");
+                break;
+            case 10:
+                Console.WriteLine("O mês é Outubro");
+                break;
+            case 11:
+                Console.WriteLine("O mês é Novembro");
+                break;
+            case 12:
+                Console.WriteLine("O mês é Dezembro");
+                break;
+            default:
+                Console.WriteLine("Mês inválido");
+                break;
         }
+        Console.ReadLine();
     }
 }
