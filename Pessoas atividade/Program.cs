5387db0d1b0c1ec228ba576b9a5f50c6cb5f88db
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-27 10:46:55 -0300

    novo projesto , para contar idade de pessoas com maior de 18 anos e menor

---
+++ Pessoas atividade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pessoas_atividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int contador = 0, aprovado = 0, reprovado = 0;
+
+            do
+            {
+                Console.WriteLine("Digite a idade de " + (10 - contador) + " pessoas");
+                int idadeEscreva = int.Parse(Console.ReadLine());
+
+                if(idadeEscreva >= 18){
+                    Console.WriteLine("Aprovado");
+                    aprovado = aprovado + 1;
+                }else if (idadeEscreva <= 17){
+                    Console.WriteLine("Reprovado");
+                    reprovado = reprovado + 1;
+                }else{
+                    Console.WriteLine("Operação inválida");
+                    break;
+                }   
+                contador = contador + 1;      
+            } while (contador < 10);
+
+            Console.WriteLine("pessoas com idade maior que 18 = "+aprovado+" pessoas com idade menor que 18 = "+reprovado);
+        }
+    }
+}
