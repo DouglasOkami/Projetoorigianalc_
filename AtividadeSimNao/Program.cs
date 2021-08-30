34bdeae4c8e9fa8aed09a61804db7e6f06efcb71
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-30 13:18:46 -0300

    projeto AtividadeSImNao terminado

--- AtividadeSimNao/Program.cs
+++ AtividadeSimNao/Program.cs
@@ -6,12 +6,39 @@ namespace AtividadeSimNao
     {
         static void Main(string[] args)
         {//começo
-            int contador = 0;
+            int contPessoas = 0, contHomens = 0, contMulheres = 0, simHomem = 0 , simMulher = 0 ;
+            char sexo , resposta;
 
             do
             {
-                
-            } while (true);//fim while
+                Console.WriteLine("Digite M para masculino ou F para feminino");
+                sexo = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite S para sim que gostou do produto e N para não gostou do produto");
+                resposta = char.Parse(Console.ReadLine());
+
+                if(sexo == 'M'){
+                    contHomens ++;
+
+                    if(resposta == 'S'){
+                        simHomem++;
+                    }
+                }else{// feminino
+                    contMulheres ++;
+
+                    if(resposta == 'S'){
+                        simMulher++;
+                    }
+                }// fim primeiro else
+
+                // fim segundo else
+                contPessoas ++;
+            } while (contPessoas < 4);//fim while
+
+            Console.WriteLine($"A quantidade de pessoas que falaram sim = {simHomem + simMulher }");
+            Console.WriteLine($"A quantidade de pessoas que falatam não = {contPessoas - (simHomem + simMulher)}");
+            Console.WriteLine($"A quantidade de mulheres que falaram sim = {simMulher} ");
+            Console.WriteLine($"A porcentagem de homens que responderam NÃO entre todos os homens analisados = {(((double)contHomens - (double)simHomem) / (double)contHomens)  * 100}%");
 
         }//fim
     }
