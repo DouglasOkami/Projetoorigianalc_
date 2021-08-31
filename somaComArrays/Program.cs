7bb7598602c4327fbc7140e82cd65401a41a7b7b
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-08-31 20:44:20 -0300

    novos projetos

---
+++ somaComArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace somaComArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
