ed5c494b3cb9a23f1a47985f4fb1353ea44dd3f2
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-09-01 08:23:36 -0300

    organizando projetos por números

---
+++ projeto4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace imparPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int[] par = new int[6];
+            int[] numero = new int[6];
+            int contador = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º de 6 numeros.");
+                numero[i] = int.Parse(Console.ReadLine());
+                if ((numero[i] % 2) == 0)
+                {
+                    par[contador] = numero[i];
+                    contador++;
+                }
+            }
+
+            Console.WriteLine($"Os numeros pares são :");
+            for (int i = 0; i < contador; i++)
+            {
+                Console.Write($"{par[i]}, ");
+            }   
+        }
+    }
+}
