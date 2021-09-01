cb5a8514d7fc6ed12582fa6556bd8b70d8fd77cf
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-09-01 08:14:12 -0300

    Tarefa feita do Projeto somaComArrays

--- somaComArrays/Program.cs
+++ somaComArrays/Program.cs
@@ -4,9 +4,20 @@ namespace somaComArrays
 {
     class Program
     {
+   
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numerosInteiros = new int[10];
+            int soma = 0;
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Insira o {i + 1}º de 10 valores");
+                numerosInteiros[i] = int.Parse(Console.ReadLine());
+            
+                soma += numerosInteiros[i]; 
+            }
+                Console.WriteLine($"a soma dos valores é: {soma}");
         }
     }
 }
