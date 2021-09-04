61fc968990bedff21d4433a5925a515412e944aa
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-09-04 18:27:21 -0300

    projeto9 finalizado e projeto10 criado

---
+++ projeto9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace projeto9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoAtual = 0,anoDeNascimento = 0;
+            int idade = anoAtual - anoDeNascimento;
+
+            Console.WriteLine("Digite o ano em que você nasceu ??");
+            anoDeNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano atual ??");
+            anoAtual = int.Parse(Console.ReadLine());
+
+            if(idade > 18){
+                Console.WriteLine("Você pode votar :)");
+            }
+            else if(idade < 18){
+                Console.WriteLine("Desculpe mais você não tem idade pra votar ainda");
+            }
+        }//Fim main
+    }
+}
